5a153b173dd4223f426dfdd454fd41bebfc6a45b
Denis Golubev <dgolubev.public@gmail.com>
2024-03-02 19:34:17 +0100

    Add metrics endpoint (for Prometheus)

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"io"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+)
+
+var (
+	totalRobotsTxtRequests = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "total_robots_txt_requests",
+		Help: "The total number of requests for the robots.txt file provided by this service.",
+	})
+	totalRobotsTxtRequestErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "total_robots_txt_request_errors",
+		Help: "The total number of errors when serving the robots.txt file. The reason for this is mostly" +
+			" the underlying robots.txt not being served properly or network issues. Not all errors may have" +
+			" been reflected back to the client, for example if an error occurred after the response has" +
+			" been started.",
+	})
 )
 
 type robotsHandler struct {
@@ -30,6 +47,8 @@ func newRobotsHandler(cfg config, logger *slog.Logger) robotsHandler {
 func (rh robotsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	rh.logger.Debug("received request")
 
+	totalRobotsTxtRequests.Inc()
+
 	originalRobotsRequest := &http.Request{
 		Method: http.MethodGet,
 		URL:    rh.cfg.originalRobotsURL,
@@ -39,6 +58,8 @@ func (rh robotsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	originalRobotsResponse, err := rh.client.Do(originalRobotsRequest)
 	if err != nil {
+		totalRobotsTxtRequestErrors.Inc()
+
 		msg := "failed to get original robots.txt"
 		rh.logger.Error(msg, "error", err)
 
@@ -49,6 +70,8 @@ func (rh robotsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
+			totalRobotsTxtRequestErrors.Inc()
+
 			rh.logger.Error("failed to close original robots.txt response body", "error", err)
 			// At this point the (error) response may already have been sent, so no response is provided
 			// in case of this error.
@@ -62,6 +85,8 @@ func (rh robotsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//       starts successfully.
 	_, err = io.Copy(w, originalRobotsResponse.Body)
 	if err != nil {
+		totalRobotsTxtRequestErrors.Inc()
+
 		msg := "failed to read original robots.txt response"
 		rh.logger.Error(msg, "error", err)
 
@@ -76,6 +101,8 @@ func (rh robotsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//       (for example to delimit a new group) can then be added by the user directly in the file.
 	_, err = fmt.Fprintln(w, rh.cfg.additionalRobotsFile)
 	if err != nil {
+		totalRobotsTxtRequestErrors.Inc()
+
 		// Errors at this point can't be reflected back to the client (as we have sent headers above already during the
 		// io.Copy call), so we just log them.
 		rh.logger.Error("failed to write additional robots.txt to response (additional lines)", "error", err)
@@ -99,9 +126,8 @@ func run(logger *slog.Logger) error {
 		"newRobotsEndpoint", cfg.newRobotsEndpoint,
 	)
 
+	http.Handle("/metrics", promhttp.Handler())
 	http.Handle(fmt.Sprintf("/%s", cfg.newRobotsEndpoint), newRobotsHandler(cfg, logger.WithGroup("newRobotsHandler")))
-
-	// TODO: add prometheus / health check endpoint?
 
 	logger.Info("listening on port", "port", cfg.port)
 	return http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
